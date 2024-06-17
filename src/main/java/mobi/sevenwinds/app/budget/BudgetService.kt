d334f481ef796d5f54a7c7baed8fa412d4f7bc03
viktor <viktor.kogai@icloud.com>
2024-06-17 10:12:20 +1000

    Fix statistics calculation and pagination in BudgetService

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -2,7 +2,9 @@ package mobi.sevenwinds.app.budget
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.sum
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object BudgetService {
@@ -21,14 +23,20 @@ object BudgetService {
 
     suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
         transaction {
+            val total = BudgetTable.select { BudgetTable.year eq param.year }.count()
+
             val query = BudgetTable
                 .select { BudgetTable.year eq param.year }
                 .limit(param.limit, param.offset)
+                .orderBy(BudgetTable.month to SortOrder.ASC, BudgetTable.amount to SortOrder.DESC)
 
-            val total = query.count()
             val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
 
-            val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
+            val sumByType = BudgetTable
+                .slice(BudgetTable.type, BudgetTable.amount.sum())
+                .select { BudgetTable.year eq param.year }
+                .groupBy(BudgetTable.type)
+                .associate { it[BudgetTable.type].name to it[BudgetTable.amount.sum()]!! }
 
             return@transaction BudgetYearStatsResponse(
                 total = total,
